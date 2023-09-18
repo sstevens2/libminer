302580b0c63aef71e97009af5bb8df0b005386c2
Sarah Stevens <sarah.stevens@wisc.edu>
2023-09-18 15:28:03 -0500

    additional funcationlity, more tests, and docs

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,18 +1,36 @@
 #' R Library Summary
 #'
-#' Provides a breif summary of this package libraries on
+#' Provides a brief summary of this package libraries on
 #' your machine
 #'
+#' @param sizes A logical value indicating whether to calculate sizes.
+#'    Default `FALSE`
+#'
 #' @return A `data.frame` containing the count of the packages in each
-#'    of the user's libraries
+#'    of the user's libraries, If `sizes = TRUE`, a column of library
+#'    sizes will be added.
 #' @export
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+#' lib_summary(sizes = TRUE)
+lib_summary <- function(sizes = FALSE) {
+  if (!is.logical(sizes)){
+    stop("sizes must be a logical value (TRUE/FALSE)")
+  }
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = F)
   names(pkg_df) <- c("Library", "n_packages")
+
+  if (isTRUE(sizes)) {
+    pkg_df$lib_size <- vapply(
+      pkg_df$Library,
+      function(x){
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      },
+      FUN.VALUE = numeric(1)
+      )
+  }
   pkg_df
 }
--- tests/testthat/test-libminer-package.R
+++ tests/testthat/test-libminer-package.R
@@ -9,5 +9,12 @@ test_that("lib_summary returns expected results",
           })
 
 test_that("lib summary fails appropriately", {
-  expect_error(lib_summary("foo", "unused argument"))
+  expect_error(lib_summary("foo"),"sizes must be a logical")
+})
+
+test_that("sizes argument works", {
+  res <- lib_summary(sizes = TRUE)
+  expect_equal(ncol(res), 3)
+  expect_equal(names(res), c("Library", "n_packages", "lib_size"))
+  expect_type(res$lib_size, "double")
 })

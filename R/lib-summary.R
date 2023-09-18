cab13783517e645fcad8c7cd9a93daed86ed4bcf
Sarah Stevens <sarah.stevens@wisc.edu>
2023-09-18 12:21:09 -0500

    Created docs for lib_summary

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,3 +1,14 @@
+#' R Library Summary
+#'
+#' Provides a breif summary of this package libraries on
+#' your machine
+#'
+#' @return A `data.frame` containing the count of the packages in each
+#'    of the user's libraries
+#' @export
+#'
+#' @examples
+#' lib_summary()
 lib_summary <- function() {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
